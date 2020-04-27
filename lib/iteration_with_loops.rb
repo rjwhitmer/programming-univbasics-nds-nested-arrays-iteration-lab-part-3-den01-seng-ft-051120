ba7e485e42acde9b04a056b7a284137e87c2ab2b
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 01:55:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,14 +3,14 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   
   row_index = 0 
-  sentence = []
+  sentence = ""
   
   while row_index < src.count do 
     element_index = 0 
     
     while element_index < src[row_index].count do 
       if src[row_index][element_index].is_a?(String)
-        sentence = sentence + src[row_index][element_index]
+        sentence = sentence.join(src[row_index][element_index])
       end
       element_index += 1 
     end
