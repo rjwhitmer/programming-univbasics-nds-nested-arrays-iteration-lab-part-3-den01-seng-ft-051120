539709e420a4a410a9d7229fe42a27a4ba1c7451
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 01:53:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
     
     while element_index < src[row_index].count do 
       if src[row_index][element_index].is_a?(String)
-        sentence += src[row_index][element_index]
+        sentence = sentence + src[row_index][element_index]
       end
       element_index += 1 
     end
