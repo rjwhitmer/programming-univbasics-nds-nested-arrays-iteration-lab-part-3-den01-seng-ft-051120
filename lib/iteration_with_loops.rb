a6f50a75b26719c4dc371c66d5b96761aec5ac5f
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 01:57:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
@@ -18,5 +20,7 @@ def join_nested_strings(src)
     row_index += 1 
     
   end
+  
+  binding.pry 
   
 end
