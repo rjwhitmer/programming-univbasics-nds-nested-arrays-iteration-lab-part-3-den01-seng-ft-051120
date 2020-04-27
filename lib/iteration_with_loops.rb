bafb0f6fb57d352dc2d44461a7764a8a4f3e81c9
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 02:10:04 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,32 @@
+require 'pry'
+
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
+  row_index = 0 
+  sentence = []
+  
+  while row_index < src.count do 
+    element_index = 0 
+    
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index].is_a?(String)
+        test = src[row_index][element_index]
+        #binding.pry
+        sentence = sentence.push(src[row_index][element_index])
+      end
+      #binding.pry
+      element_index += 1 
+    end
+    
+    row_index += 1 
+    
+  end
+  
+  new_sentence = sentence.join(" ")
+  new_sentence
+  #binding.pry 
+   
+  
 end
